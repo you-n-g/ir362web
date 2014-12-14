1b3c5afb717137a1d5d228f4997065520ab202b8
ZhuQichen <ZhuQichen@users.noreply.github.com>
2014-12-15 00:40:02 +0800

    previewer.js

--- previewer/previewer.js
+++ previewer/previewer.js
@@ -1,3 +1,20 @@
+/*
+ * Author: Zhu Qichen
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 var server = require('webserver').create();
 var WIDTH = 588;
 var HEIGHT = 833;
