f9c22737c6de54064a0a2f4e28f98f780c5ac7b6
viczzx <viczzx@ubuntu14.04>
2014-12-15 00:12:32 +0800

    add cluster function

--- autocompletor/autocompletor/urls.py
+++ autocompletor/autocompletor/urls.py
@@ -3,8 +3,10 @@ from django.contrib import admin
 
 from autocompletor.views import get_match
 from autocompletor.views import update_query
+from autocompletor.views import cluster
 
 urlpatterns = patterns('',
     url(r'^get_match/([^/]*)$', get_match),
     url(r'^update_query/([^/]*)$', update_query),
+    url(r'^cluster/$', cluster),
 )
