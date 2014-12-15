7be52e6e6ad57111ac12c9fe061d8b7f65a838d4
viczzx <viczzx@ubuntu14.04>
2014-12-15 13:57:05 +0800

    random cluster

--- autocompletor/autocompletor/views.py
+++ autocompletor/autocompletor/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
+import random
 import json
 import time
 from auto_completor import auto_completor
@@ -23,11 +24,35 @@ def update_query(request, query=""):
             results.append(r[0])
     result = json.dumps(results)
     return HttpResponse(result)
+
+def _cluster(docIDs):
+    group1 = []
+    group2 = []
+    group3 = []
+    group4 = []
+    count = 0
+    while count < len(docIDs):
+        if count % 3 == 0:
+            group1.append(docIDs[count])
+        elif count % 3 == 1:
+            group2.append(docIDs[count])
+        else:
+            group3.append(docIDs[count])
+        count += 1
+    res = {"group1": group1,
+            "group2": group2,
+            "group3": group3}
+    return res
+
 @csrf_exempt
 def cluster(request, req=""):
     post = request.POST
     docIDs = post.get('docIDs', None)
+    res = {}
+    if not docIDs:
+        return HttpResponse(json.dumps(res))
     docIDs = json.loads(docIDs)
-    res = auto_completor.cluster(docIDs)
+    #res = auto_completor.cluster(docIDs)
+    res = _cluster(docIDs)
     res = json.dumps(res)
     return HttpResponse(res)
